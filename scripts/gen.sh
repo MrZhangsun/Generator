878cb6025befae664c5274335bd6ec747cc9797b
Murphy Zhang Sun <mr_zhangsun@outlook.com>
2019-08-10 10:50:44 +0800

    add mapper config

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -1,2 +1,5 @@
 java -jar ./mybatis-generator-core-1.3.2.jar -configfile ./configs/generator.xml -overwrite
 java -jar ./mybatis-generator-core-1.3.2.jar -configfile ./configs/generator-lease-company.xml -overwrite
+java -jar ./mybatis-generator-core-1.3.2.jar -configfile ./configs/generator-fence-company.xml -overwrite
+java -jar ./mybatis-generator-core-1.3.2.jar -configfile ./configs/generator-yxt-latest.xml -overwrite
+java -jar ./mybatis-generator-core-1.3.2.jar -configfile ./configs/generator-yxt-alarm.xml -overwrite
