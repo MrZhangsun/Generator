83f15f933643244ddb3096d2b38e7137642b743a
Murphy Zhang Sun <mr_zhangsun@outlook.com>
2019-08-10 13:30:03 +0800

    add scripts

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -1,5 +1,9 @@
 java -jar ./mybatis-generator-core-1.3.2.jar -configfile ./configs/generator.xml -overwrite
 java -jar ./mybatis-generator-core-1.3.2.jar -configfile ./configs/generator-lease-company.xml -overwrite
-java -jar ./mybatis-generator-core-1.3.2.jar -configfile ./configs/generator-fence-company.xml -overwrite
+java -jar ./mybatis-generator-core-1.3.2.jar -configfile ./configs/generator-fence.xml -overwrite
+
 java -jar ./mybatis-generator-core-1.3.2.jar -configfile ./configs/generator-yxt-latest.xml -overwrite
+java -jar ./mybatis-generator-core-1.3.2.jar -configfile ./configs/generator-yxt-terminal.xml -overwrite
+java -jar ./mybatis-generator-core-1.3.2.jar -configfile ./configs/generator-terminal-bind.xml -overwrite
 java -jar ./mybatis-generator-core-1.3.2.jar -configfile ./configs/generator-yxt-alarm.xml -overwrite
+java -jar ./mybatis-generator-core-1.3.2.jar -configfile ./configs/generator-yxt-position.xml -overwrite
